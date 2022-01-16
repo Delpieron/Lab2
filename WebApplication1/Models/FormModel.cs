1c77af774cccba8128fb14d33e057dbec8389813
Krzysiek Wróblewski <krzysiuw1906@gmail.com>
2022-01-16 14:17:08 +0100

    Add project files.

---
+++ WebApplication1/Models/FormModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab2.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+        [Display(Name = "Data Urodzenia")]
+        public DateTime BirthDate { get; set; }
+    }
+}
